bf586071852d68fcc096c3c1736d522abcd0661e
caiotarso <caiotarso7@gmail.com>
2025-08-26 15:24:19 -0300

    feat(main): adicionando leitor de csv e organizando design

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type GameState struct {
 	PlayerName string
-	PlayerPoints string
+	PlayerPoints int
 	Questions []Question
 }
 
@@ -31,12 +33,58 @@ func (g *GameState) Init() {
 
 	g.PlayerName = name
 
-	fmt.Printf("Ok! Vamos começar o Quiz, %s", g.PlayerName)
+	fmt.Printf("Ok! Vamos começar o Quiz, %s \n", g.PlayerName)
+}
+
+func (g *GameState) ProcessCSV() {
+	f, error := os.Open("quizgolang.csv")
+	if error != nil {
+		panic("Erro ao ler arquivo")
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic("Erro ao ler arquivo")
+	}
+    
+	for index, record := range records {
+		if index > 0 {
+			question := Question{
+				Text: record[0],
+				Options: record[1:5],
+				CorrectAnswer: toInt(record[5]),
+		}
+
+		g.Questions = append(g.Questions, question)
+	}
+
+ }
+}
+
+func (g *GameState) Run() {
+	for index, question := range g.Questions {
+		fmt.Println(index+1, question.Text)
+	}
 }
 
 func main() {
-    gameteste := &GameState{}
+    gameteste := &GameState{PlayerPoints: 0}
+	go gameteste.ProcessCSV()
 	gameteste.Init()
+	gameteste.Run()
+	
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic("Erro ao converter string para int")
+	}
+
+	return i
 }
 
 
